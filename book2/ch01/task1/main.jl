cde360c5e55f0c631b513ccb50cb54c858e655a3
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-28 19:13:59 +0100

    finished solution to book2/ch01/task1 (added handling user input, etc.)

--- book2/ch01/task1/main.jl
+++ book2/ch01/task1/main.jl
@@ -1,3 +1,28 @@
+function get_integer_from_user(number_designation::String="a")::Integer
+    int_from_user::Integer = 0
+    while true
+        print("\nEnter number " * number_designation * " (integer): ")
+        try
+            int_from_user = parse(Int64, readline())
+            break
+        catch err
+            if isa(err, ArgumentError)
+                println("That was not an integer. Try again.")
+            end
+        end
+    end
+    int_from_user
+end
+
+function print_program_description()::Nothing
+    println("\nHi.\n")
+    println("This is a program that reads two integers, a and b, from the user.")
+    println("Next it will perform a number of mathematical operations on them.")
+    println("The results of the operations will be printed to the screen.")
+    println("NO GUARANTEE OF THE PROGRAM ACCURACY. Still, I hope it'll work fine.")
+    println("All clear. Then let's begin.\n")
+end
+
 function main()
     customFunctions = [
         Dict("description" => "adds a and b",
@@ -14,15 +39,14 @@ function main()
             "fn" => (a, b) -> a^b),
     ]
 
-    println("-"^30)
-    println("Task1.")
-    println("Custom function examples.")
+    print_program_description()
+    int_a::Integer = get_integer_from_user()
+    int_b::Integer = get_integer_from_user("b")
     for customFn in customFunctions
         println("\nDescription: $(customFn["description"])")
-        println("fn(20, 3) = $(customFn["fn"](20, 3))}")
+        println("fn($(int_a), $(int_b)) = $(customFn["fn"](int_a, int_b))")
     end
-    println("\nThat's all. Goodbye!")
-    println("-"^30)
+    println("\nThat's all. Goodbye!\n")
 end
 
 main()
