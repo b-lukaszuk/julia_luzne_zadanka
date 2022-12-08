010be5cacaaed23446b81097a365ce04c3fcc6a2
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-12-08 19:50:11 +0100

    added solution to book2/ch02/task4

--- book2/ch02/task4.jl
+++ book2/ch02/task4.jl
@@ -4,6 +4,9 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ fef0259f-3851-45c2-b530-ffe8837e1c78
+using PlutoUI
+
 # ╔═╡ 605d94a6-7723-11ed-1b6a-b19f8db19c16
 md"""# Task 4
 
@@ -48,5 +51,52 @@ Pay 00
 </pre>
 """
 
+# ╔═╡ 04067ce1-3200-4bc8-b7c8-716e71be6f4c
+md"""## Solution
+NO GUARANTEE IT WILL WORK OR WORK CORRECTLY! USE IT AT YOUR OWN RISK!
+"""
+
+# ╔═╡ addad8c5-ab7b-4968-9a52-403d81e3c113
+# result: -1:36, where -1 is 00
+function spin_roulette()::Int64
+	rand(-1:36)
+end
+
+# ╔═╡ 0d79fad1-6367-4f38-a9d7-7675fcec2f1e
+function spin2string(spin::Int64)::String
+	(spin < 0) ? "00" : string(spin)
+end
+
+# ╔═╡ 914ea9cb-6dd4-4e81-be4f-80d7fb808e59
+function is_red(spin::Int64)::Bool
+	spin in [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+end
+
+# ╔═╡ c5aac29e-5606-46f2-b8f5-373c78860cc9
+function get_spin_and_pays_msgs(spin::Int64)::String
+	spin_str::String = spin2string(spin)
+	msg::String = "The pay resulted in $spin_str...\n"
+	msg *= "Pay $spin_str\n"
+	if (spin <= 0)
+		return msg
+	end
+	is_red(spin) ? msg *= "Pay Red\n" : msg *= "Pay Black\n"
+	(spin % 2 == 0) ? msg *= "Pay Even\n" : msg *= "Pay Odd\n"
+	(spin <= 18) ? msg *= "Pay 1-18\n" : msg *= "Pay 19-36\n"
+	return msg
+end
+
+# ╔═╡ a5482c3a-bf65-4baa-a458-7c7def6a9d27
+with_terminal() do
+	println(get_spin_and_pays_msgs(spin_roulette()))
+end
+
 # ╔═╡ Cell order:
 # ╟─605d94a6-7723-11ed-1b6a-b19f8db19c16
+# ╟─04067ce1-3200-4bc8-b7c8-716e71be6f4c
+# ╠═fef0259f-3851-45c2-b530-ffe8837e1c78
+# ╠═addad8c5-ab7b-4968-9a52-403d81e3c113
+# ╠═0d79fad1-6367-4f38-a9d7-7675fcec2f1e
+# ╠═914ea9cb-6dd4-4e81-be4f-80d7fb808e59
+# ╠═c5aac29e-5606-46f2-b8f5-373c78860cc9
+# ╠═a5482c3a-bf65-4baa-a458-7c7def6a9d27
