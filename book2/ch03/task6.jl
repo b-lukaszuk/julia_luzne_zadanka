27c417c0457ce9e26462f78365ea8bb093bd2218
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-12-15 10:11:01 +0100

    cosmetic changes (code formatting)

--- book2/ch03/task6.jl
+++ book2/ch03/task6.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.7
+# v0.14.9
 
 using Markdown
 using InteractiveUtils
@@ -28,14 +28,14 @@ NO GUARANTEE IT WILL WORK OR WORK CORRECTLY! USE IT AT YOUR OWN RISK!
 
 # ╔═╡ 5935469b-cb15-4de7-a3dc-2b3892ce6b3b
 function contains_only_1_or_0(maybe_binary::String)::Bool
-	length(filter(x -> !(x == '0' || x == '1'), maybe_binary)) == 0
+    length(filter(x -> !(x == '0' || x == '1'), maybe_binary)) == 0
 end
 
 # ╔═╡ 9b0fdde8-243e-4681-80e6-367ab966d3ba
 function binary_2_decimal(binary::String)::Integer
-	if !contains_only_1_or_0(binary)
-		throw(ArgumentError("binary can conatin only 1s and 0s"))
-	end
+    if !contains_only_1_or_0(binary)
+	throw(ArgumentError("binary can conatin only 1s and 0s"))
+    end
     decimal::Integer = 0
     rev_binary::String = binary[end:-1:1]
     for i in eachindex(rev_binary)
@@ -46,7 +46,7 @@ end
 
 # ╔═╡ 3279f9fc-1677-46fd-af43-718c0d740a87
 function get_random_binary(binary_length::Integer = 6)
-	join(rand(("0", "1"), binary_length), "")
+    join(rand(("0", "1"), binary_length), "")
 end
 
 # ╔═╡ 6b617524-8395-444b-8cd1-4a2f3a301df5
@@ -54,13 +54,13 @@ binaries_examples::Array{String} = [get_random_binary() for _ in 1:5]
 
 # ╔═╡ f1fc75cb-c740-4466-abb4-d82a83c7d174
 with_terminal() do
-	for tested_binary in binaries_examples
-		println("-" ^ 3)
-		println("Converting binary to decimal my function.")
-		println("$tested_binary => $(binary_2_decimal(tested_binary))")
-		println("Converting binary to decimal build-in function.")
-		println("$tested_binary => $(parse(Int, tested_binary; base=2))")
-	end
+    for tested_binary in binaries_examples
+	println("-" ^ 3)
+	println("Converting binary to decimal my function.")
+	println("$tested_binary => $(binary_2_decimal(tested_binary))")
+	println("Converting binary to decimal build-in function.")
+	println("$tested_binary => $(parse(Int, tested_binary; base=2))")
+    end
 end
 
 # ╔═╡ Cell order:
