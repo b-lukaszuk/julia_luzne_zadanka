eb06a6c674773a3158b27be43b43d61cb0f51560
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-02-11 13:28:42 +0100

    added code for book3/ch09/ex.9.5

--- book3/ch09/ch09.jl
+++ book3/ch09/ch09.jl
@@ -26,3 +26,16 @@ ht.SignedRankTest(tab91[!, "daily_intake_kJ"] .- 7725)
 # to get p-value itself from a test:
 # x = ht.SignedRankTest(tab91[!, "daily_intake_kJ"] .- 7725)
 # ht.pvalue(x)
+
+
+##########################################
+# Ch 9.5 TWO GROUPS OF PAIRED OBSERVATIONS
+##########################################
+tab93 = pd.DataFrame(
+    (; intake_kJ_premenopause=[5260, 5470, 5640, 6180, 6390, 6515, 6805, 7515, 7515, 8230, 8770],
+    intake_kJ_postmenopause=[3910, 4220, 3885, 5160, 5645, 4680, 5265, 5975, 6790, 6900, 7335]
+))
+
+ht.OneSampleTTest(tab93[!, :intake_kJ_premenopause], tab93[!, :intake_kJ_postmenopause])
+ht.SignTest(tab93[!, :intake_kJ_premenopause] .- tab93[!, :intake_kJ_postmenopause])
+ht.SignedRankTest(tab93[!, :intake_kJ_premenopause] .- tab93[!, :intake_kJ_postmenopause])
