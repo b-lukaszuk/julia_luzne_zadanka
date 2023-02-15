93e1f30b35db2948d5f8c0006e533b05ef8a16c4
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-02-15 10:13:56 +0100

    added functions.jl with a few helper functions

---
+++ book3/ch09/functions.jl
@@ -0,0 +1,29 @@
+import HypothesisTests as ht
+import MultipleTesting as mt
+import Pingouin as pg
+
+function get_avg(vals::Vector{<:Number})::Float64
+    return sum(vals) / length(vals)
+end
+
+function get_sd(vals::Vector{<:Number})::Float64
+    avg::Float64 = get_avg(vals)
+    diffs_squared::Vector{<:Float64} = [(v - avg)^2 for v in vals]
+    return sqrt(sum(diffs_squared) / (length(vals) - 1))
+end
+
+function are_eql_vars(vals1::Vector{<:Number}, vals2::Vector{<:Number}, alpha::Float64=0.05)::Bool
+    return ht.pvalue(ht.LeveneTest(vals1, vals2)) >= alpha
+end
+
+function are_all_norm_distributed(vals::Vector{<:Number}...)::Bool
+    p_val::Float64 = 0
+    ALPHA::Float64 = 0.05
+    for v in vals
+        p_val = pg.normality(v).pval[1]
+        if (p_val < ALPHA)
+            return false
+        end
+    end
+    return true
+end
