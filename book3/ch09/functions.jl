6ed4ba4d21e5c6269921da354c3514885c129e7f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-02-15 10:34:14 +0100

    cosmetic changes

--- book3/ch09/functions.jl
+++ book3/ch09/functions.jl
@@ -12,16 +12,15 @@ function get_sd(vals::Vector{<:Number})::Float64
     return sqrt(sum(diffs_squared) / (length(vals) - 1))
 end
 
-function are_eql_vars(vals1::Vector{<:Number}, vals2::Vector{<:Number}, alpha::Float64=0.05)::Bool
+function are_eql_vars(vals1::Vector{<:Number}, vals2::Vector{<:Number}; alpha::Float64=0.05)::Bool
     return ht.pvalue(ht.LeveneTest(vals1, vals2)) >= alpha
 end
 
-function are_all_norm_distributed(vals::Vector{<:Number}...)::Bool
+function are_all_norm_distributed(vals::Vector{<:Number}...; alpha::Float64=0.05)::Bool
     p_val::Float64 = 0
-    ALPHA::Float64 = 0.05
     for v in vals
         p_val = pg.normality(v).pval[1]
-        if (p_val < ALPHA)
+        if (p_val < alpha)
             return false
         end
     end
