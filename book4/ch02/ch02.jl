d427cb9495bf4ce64e3396ecbbdcb7d0a1170268
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-02-19 20:03:44 +0100

    added description/solution to book4/ch02/ex4

--- book4/ch02/ch02.jl
+++ book4/ch02/ch02.jl
@@ -312,6 +312,36 @@ begin
 	ex32
 end
 
+# ╔═╡ cceae031-a9a5-4a1d-b2bb-96f71da9e566
+md"""### Exercise 4
+M&M’s are small candy-coated chocolates that come in a variety of colors.
+Mars, Inc., which makes M&M’s, changes the mixture of colors from time to time. In 1995, they introduced blue M&M’s.
+- In 1994, the color mix in a bag of plain M&M’s was 30% Brown, 20% Yellow, 20% Red, 10% Green, 10% Orange, 10% Tan.
+- In 1996, it was 24% Blue , 20% Green, 16% Orange, 14% Yellow, 13% Red, 13% Brown.
+
+Suppose a friend of mine has two bags of M&M’s, and he tells me that one is from 1994 and one from 1996. He won’t tell me which is which, but he gives me one M&M from each bag. One is yellow and one is green. What is the probability that the yellow one came from the 1994 bag?
+
+Hint: The trick to this question is to define the hypotheses and the data carefully.
+"""
+
+# ╔═╡ 8ec7c9b8-c7a9-4753-867a-6188983b65cd
+ex4 = pd.DataFrame(
+	(; hypothesis_no=[1, 2],
+	hypothesis_state=["bg1994_bg1996", "bg1996_bg1994"],
+	# prior is P(H)
+	prior=[1//2, 1//2],
+	# likelihood is P(D|H), so I assume the hypothesis is true
+	# y-g, y-g
+	likelihood=[0.2*0.2, 0.14*0.1]
+	)
+)
+
+# ╔═╡ 00e0ad77-b583-493a-88c6-f6197cf55c20
+begin
+	update!(ex4)
+	ex4
+end
+
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -773,5 +803,8 @@ version = "17.4.0+0"
 # ╠═49b548b0-ffd2-4bad-a57d-552d6d639a24
 # ╠═0af84d8d-4b77-4f33-81bd-20f53eafc5c5
 # ╠═4cd525ba-24a9-4bd0-9780-a28bdbd7aeee
+# ╟─cceae031-a9a5-4a1d-b2bb-96f71da9e566
+# ╟─8ec7c9b8-c7a9-4753-867a-6188983b65cd
+# ╠═00e0ad77-b583-493a-88c6-f6197cf55c20
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
