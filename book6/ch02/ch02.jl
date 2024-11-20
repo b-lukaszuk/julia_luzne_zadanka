04269a6864dacbc14787ecdee745fada8723cfef
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-11-20 23:01:25 +0100

    added # In [XX] comments for rough orientation in text

--- book6/ch02/ch02.jl
+++ book6/ch02/ch02.jl
@@ -8,27 +8,28 @@ const Vec = Vector
 # 2.3 Lab: Introduction to Python
 
 ## 2.3.2 Basic Commands
-println("Fit a model with ", 11, " variables")
-3+5
-"hello" * " " * "world"
-x = [3, 4, 5]
-y = [4, 9, 7]
-z = vcat(x, y) # creates a copy of x and y
+println("Fit a model with ", 11, " variables") # In [1]
+3+5 # In [3]
+"hello" * " " * "world" # In [4]
+x = [3, 4, 5] # In [5]
+
+y = [4, 9, 7] # In [6]
+# creates a copy of x and y
+z = vcat(x, y) # In [6]
 
 ## 2.3.3 Introduction to Numerical Python
-x = [3, 4, 5]
-y = [4, 9, 7]
-x .+ y
+x = [3, 4, 5] # In [8]
+y = [4, 9, 7] # In [8]
+x .+ y # In [9]
 
-x = [1 2; 3 4]
-ndims(x)
-typeof(x)
+x = [1 2; 3 4] # In [10]
+ndims(x) # In [11]
+typeof(x), typeof(x[1]) # In [12]
 
-typeof([1 2; 3.0 4])
-Matrix{Float64}([1 2; 3 4])
-size(x)
-sum(x)
+size(x) # In [16]
+sum(x) # In [17]
 
+# In [19]
 x = collect(1:6)
 x
 x_reshaped = reshape(x, (2, 3)) # colum major, not row major
@@ -61,30 +62,36 @@ reshapeVec(x, 2, 3, true)
 x_reshaped[1, 1]
 x_reshaped[2, 3]
 
+# In [23]
 my_tuple = (3, 4, 5)
 my_tuple[1] = 2 # error, tuples cannot be changed after created
 
-sqrt.(x)
-x .^ 2
-x .^ 0.5 # same as sqrt.(x)
+sqrt.(x) # In [25]
+x .^ 2 # In [26]
+x .^ 0.5 # In [27]
 
 # random numbers from N(0, 1)
-x = Rand.rand(Dsts.Normal(), 50)
-y = x .+ Rand.rand(Dsts.Normal(50, 1), 50)
-Stats.cor(x, y)
+x = Rand.rand(Dsts.Normal(), 50) # In [28]
+y = x .+ Rand.rand(Dsts.Normal(50, 1), 50) # In [29]
+Stats.cor(x, y) # In [30]
 
+# In [31]
 # diffrent random nums
 Rand.rand(Dsts.Normal(0, 5), 2)
 Rand.rand(Dsts.Normal(0, 5), 2)
 
+# In [32]
 # the same random nums
 Rand.seed!(1303)
 Rand.rand(Dsts.Normal(0, 5), 2)
 Rand.seed!(1303)
 Rand.rand(Dsts.Normal(0, 5), 2)
 
+# In [33]
 Rand.seed!(3)
 y = Rand.rand(Dsts.Normal(), 10)
+
+# In [34]
 # Stats.var(y) == Stats.var(y, corrected=true)
 Stats.var(y), Stats.std(y), Stats.mean(y)
 
@@ -94,9 +101,11 @@ y = Rand.rand(Dsts.Normal(), (10, 3))
 y = [1 2 3; 4 5 6]
 Stats.mean(y)
 Stats.mean(y, dims=1) # col-major first
+# In [37]
 Stats.mean(y, dims=2) # row-major second
 
 # 2.3.4 Graphics
+# In [39-46]
 x = Rand.rand(Dsts.Normal(), 100)
 y = Rand.rand(Dsts.Normal(), 100)
 fig = Cmk.Figure();
@@ -112,9 +121,11 @@ ax2 = Cmk.Axis(fig[2, 3],
 Cmk.scatter!(ax2, x, y, marker=:cross);
 fig
 
+# In [47]
 #Cmk.save("figure.png", fig)
 #Cmk.save("figure.pdf", fig)
 
+# In [49]
 # contourplots
 x = range(-π, π, 50)
 y = range(-π, π, 50)
@@ -122,18 +133,22 @@ y = range(-π, π, 50)
 # outer product (≈ np.multiply.outer) of vectors x and y is: x *- y'
 f = cos.(y) *- (1 ./ (1 .+ x.^2))'
 
+# In [49] c.d.
 # x and y axes are swapped compared to python
 fig = Cmk.Figure();
 ax = Cmk.Axis(fig[1, 1]);
 Cmk.contour!(ax, x, y, f);
 fig
 
+# In [50]
 # x and y axes are swapped compared to python
 fig = Cmk.Figure();
 ax = Cmk.Axis(fig[1, 1]);
 Cmk.contour!(ax, f; levels=45);
 fig
 
+# In [51]
+# x and y axes are swapped compared to python
 fig = Cmk.Figure();
 ax = Cmk.Axis(fig[1, 1]);
 Cmk.contourf!(ax, f; levels=100, colormap=:viridis);
