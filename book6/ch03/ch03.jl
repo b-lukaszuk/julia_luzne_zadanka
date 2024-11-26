83ff7e6c53a5135a715fd56110d7328288d0fce1
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-11-26 20:45:32 +0100

    added book6/ch03.jl with initial contents

---
+++ book6/ch03/ch03.jl
@@ -0,0 +1,10 @@
+import CSV as Csv
+import CairoMakie as Cmk
+import DataFrames as Dfs
+
+const F64 = Float64
+const I64 = Int64
+const Str = String
+const Vec = Vector
+
+# 3.6 Lab: Linear Regression
