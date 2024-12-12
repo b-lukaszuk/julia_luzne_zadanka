efe3e5a31de998147899d5e6fb9021a8b8532af2
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-12-12 22:58:39 +0100

    added book6/ch03.jl/3.6.1 Importing Packages

--- book6/ch03/ch03.jl
+++ book6/ch03/ch03.jl
@@ -8,3 +8,28 @@ const Str = String
 const Vec = Vector
 
 # 3.6 Lab: Linear Regression
+
+## 3.6.1 Importing Packages
+
+# In [2]
+# https://www.statsmodels.org/stable/index.html
+
+# In [3]
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.outliers_influence.variance_inflation_factor.html
+# https://www.statsmodels.org/stable/generated/statsmodels.stats.anova.anova_lm.html
+
+# In [4]
+# https://github.com/jonathan-taylor/ISLP/blob/master/ISLP/__init__.py
+# https://github.com/jonathan-taylor/ISLP/blob/master/ISLP/models/__init__.py
+
+# In [5]
+names(Base)
+names(Core)
+varinfo(Base)
+varinfo(Core)
+
+# In [6]
+a = [3, 5, 11]
+
+# In [7]
+sum(a)
