ade915701894cd3410770d413aa6651669aeb827
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-03-02 21:17:13 +0100

    added book6/ch03/3.6.2 (part of)

--- book6/ch03/ch03.jl
+++ book6/ch03/ch03.jl
@@ -1,6 +1,7 @@
 import CSV as Csv
 import CairoMakie as Cmk
 import DataFrames as Dfs
+import GLM as Glm
 
 const Flt = Float64
 const Int = Int64
@@ -33,3 +34,26 @@ a = [3, 5, 11]
 
 # In [7]
 sum(a)
+
+
+## 3.6.2 Simple Linear Regression
+
+# In [8]
+# dataset description
+# https://islp.readthedocs.io/en/latest/datasets/Boston.html
+boston = Csv.read("./Boston.csv", Dfs.DataFrame);
+Dfs.select!(boston, Dfs.Not(:Column1));
+names(boston)
+
+# In [9]
+# lstat: lower status of the population (percent).
+nRows, nCols = size(boston);
+x = Dfs.DataFrame(Dict(
+    "intercept" => ones(Flt, nRows),
+    "lstat" => boston.lstat));
+x[1:4, :]
+
+# In [10-11]
+# medv: median value of owner-occupied homes in $1000s.
+x.medv = boston.medv
+model = Glm.lm(Glm.@formula(medv ~ lstat), x)
