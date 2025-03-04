c3ed7ea913b9df585f69b7422d4ee4d7fdd07fe4
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-03-04 20:43:45 +0100

    added book6/ch03/3.6.2, subsec Fit and Transform

--- book6/ch03/ch03.jl
+++ book6/ch03/ch03.jl
@@ -28,6 +28,7 @@ names(Base)
 names(Core)
 varinfo(Base)
 varinfo(Core)
+varinfo()
 
 # In [6]
 a = [3, 5, 11]
@@ -55,5 +56,37 @@ x[1:4, :]
 
 # In [10-11]
 # medv: median value of owner-occupied homes in $1000s.
-x.medv = boston.medv
+x.medv = boston.medv # response variable
 model = Glm.lm(Glm.@formula(medv ~ lstat), x)
+
+### Using Transformations: Fit and Transform
+
+# In [12-13]
+design = Dfs.DataFrame(Dict(
+    "intercept" => ones(Flt, nRows),
+    "lstat" => boston.lstat));
+first(design, 4)
+
+# In [14]
+Glm.coeftable(model)
+
+# In [15]
+for (cn, cv) in zip(Glm.coefnames(model), Glm.coef(model))
+    println("$(rpad(cn, 15))$cv")
+end
+
+# In [16]
+new_df = Dfs.DataFrame((lstat=[5, 10, 15]))
+
+# In [17]
+Glm.predict(model, new_df)
+
+# In [18]
+Glm.predict(model, new_df, interval=:confidence, level=0.95)
+# interval=:confidence, 95% CI for predicted mean
+# https://en.wikipedia.org/wiki/Confidence_interval
+
+# In [19]
+Glm.predict(model, new_df, interval=:prediction, level=0.95)
+# interval=:prediction, 95% CI for predicted data points
+# https://en.wikipedia.org/wiki/Prediction_interval
